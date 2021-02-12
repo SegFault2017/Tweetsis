96594befa9b876ecf5b2e6eca1206e657ba202da
Raymond Tan <ray.tangent42@gmail.com>
2021-02-12 14:08:41 -0500

    CSP bug on depolyment.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import express,{Express} from "express";
+import express,{Express, Request, Response} from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
 import { notFound, errHandler } from "./middlewares/errors";
 import router from "./router";
+import path from "path";
 
 class App{
   private app: Express;
@@ -24,8 +25,12 @@ class App{
   	this.app.use(express.json());
   	if (process.env.NODE_ENV === "development") {
   		this.app.use(morgan("dev"));
-	  } else if(process.env.NODE_ENV === "production"){
-		  this.app.use(express.static("frontend/build"));
+		} else if (process.env.NODE_ENV === "production") {
+			console.log("In production mode.");
+			this.app.use(express.static(path.join(__dirname + "/../public")));
+			this.app.get("*", function (req: Request, res: Response) {
+				res.sendFile(path.join(__dirname + "/../public/index.html"));
+			});
 	  }
   }
 
